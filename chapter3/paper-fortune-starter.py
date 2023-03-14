0e0adde7ea4966e7630bbb0c6d24cec0bffd0374
Samuel Earl <samueltremain@gmail.com>
2023-03-14 10:35:51 -0700

    Remove comments

--- chapter3/paper-fortune-starter.py
+++ chapter3/paper-fortune-starter.py
@@ -6,10 +6,8 @@ fortune2 = "Something good is going to happen tomorrow"
 print("Welcome To Your Paper Fortune!")
 choice = input("Enter a color and press the enter key (red, blue, green, orange): ")
 
-# If blue or orange, then display the even numbered fortunes.
 if len(choice) % 2 == 0:
     print(fortune2)
-# Otherwise display the odd numbered fortunes.
 else:
     print(fortune1)
     
